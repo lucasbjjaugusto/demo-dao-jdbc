ceba612c5e10f5b2822aa95ddc12ebdcb292adad
Lucas Augusto <lucasbjjaugusto@gmail.com>
2021-07-20 17:23:23 -0300

    incrementation insert and findall

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -16,18 +17,30 @@ public class Program {
 		System.out.println(seller);
 		*/
 		SellerDao sellerDao = DaoFactory.createSellerDao();
-		System.out.println("=== Teste 1: findById ===");
+		System.out.println("=== Teste 1: Seller findById ===");
 				
 		Seller seller = sellerDao.findById(3);
 		System.out.println(seller);
 		
-		System.out.println("=== Teste 2: findById ===");
+		System.out.println("=== Teste 2: Seller findByDepartment ===");
 		Department department = new Department(2,null);
 		List<Seller> list = sellerDao.findByDepartment(department);
 		
 		for (Seller obj : list) {
 			System.out.println(obj);
 		}
+		
+		System.out.println("=== Teste 3: Seller findAll ===");
+		list = sellerDao.findAll();
+		
+		for (Seller obj : list) {
+			System.out.println(obj);
+		}
+		
+		System.out.println("=== Teste 4: Seller insert ===");
+		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
+		sellerDao.insert(newSeller);
+		System.out.println("Inserted! New id = " + newSeller.getId());
 
 	}
 
